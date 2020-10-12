9c9cb3233bb5af3f6b3146e55c2d36672720884f
Daniele Caden <contato.sophiebc@gmail.com>
2020-10-11 22:17:22 -0300

    Calculadora de números Primos

--- primos.py
+++ primos.py
@@ -3,9 +3,26 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+def ehDivisivel(n, d):
+    return n % d == 0
+
+def ehPrimo(n):
+    for d in range(2, n):
+        if (ehDivisivel(n, d)):
+            return False
+    return True
+
 @app.route("/")
 def primos():
-    resultado = "lalalala"
+    q = 100
+    c = 0
+    n = 2
+    resultado = ""
+    while c < q:
+        if ehPrimo(n):
+            c += 1
+            resultado = resultado + str(n) + ", "
+        n += 1
     return resultado
 
 if __name__ == "__main__":
